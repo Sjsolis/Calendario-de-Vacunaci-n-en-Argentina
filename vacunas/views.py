54cc64bf9eb16ca2c5ca673da7b184571aa60ac2
Silvina <silvina.solis@gmail.com>
2025-08-31 12:23:38 -0300

    Reparación de proyecto

--- vacunas/views.py
+++ vacunas/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def inicio (request):
-    return render (request,'inicio.html')
+    return render (request, 'inicio/lista_vacunas.html')
     #return HttpResponse('<h2>Vacunas del Calendario</h2>')
     
